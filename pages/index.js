502ea278fd341b824595d9745a18ccada6c3f591
Devin Lane <devin.c.lane@gmail.com>
2020-11-12 21:08:10 -0800

    nickers

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Nic Nacs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="I am the kick kacks" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hello I am nickers
         </p>
       </main>
 
